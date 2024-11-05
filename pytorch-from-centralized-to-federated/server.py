150575fc3efafd0fd12816867555be69c2d279bb
SteGala <galaxx.ste@gmail.com>
2024-11-05 18:26:48 +0100

    update

--- pytorch-from-centralized-to-federated/server.py
+++ pytorch-from-centralized-to-federated/server.py
@@ -2,10 +2,20 @@
 
 from typing import List, Tuple
 import os
+import time
+
 
 import flwr as fl
 from flwr.common import Metrics
+import requests
 
+def send_duration(ep, duration):
+    data = {"duration": duration}
+    response = requests.post(f'http://{ep}/post_duration', json=data)
+    if response.status_code == 200:
+        print('Duration sent successfully')
+    else:
+        print('Error sending duration')
 
 # Define metric aggregation function
 def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
@@ -21,17 +31,27 @@ def read_env_variable():
     server_ip = os.getenv('SERVER_IP', "0.0.0.0")
     server_port = os.getenv('SERVER_PORT', "8000")
     num_rounds = int(os.getenv('NUM_ROUNDS', 5))
+    metrics_server = os.getenv('METRICS_SERVER', "0.0.0.0:5000")
 
-    return server_ip, server_port, num_rounds
+    return server_ip, server_port, num_rounds, metrics_server
 
-server_ip, server_port, num_rounds = read_env_variable()
+if __name__ == "__main__":
+    server_ip, server_port, num_rounds, metrics_server = read_env_variable()
 
-# Define strategy
-strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=weighted_average)
+    # compute elapsed time
+    start_time = time.time()
 
-# Start Flower server
-fl.server.start_server(
-    server_address=f"{server_ip}:{server_port}",
-    config=fl.server.ServerConfig(num_rounds=num_rounds),
-    strategy=strategy,
-)
+    # Define strategy
+    strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=weighted_average)
+
+    # Start Flower server
+    fl.server.start_server(
+        server_address=f"{server_ip}:{server_port}",
+        config=fl.server.ServerConfig(num_rounds=num_rounds),
+        strategy=strategy,
+    )
+
+    # compute elapsed time
+    end_time = time.time()
+    duration = end_time - start_time
+    send_duration(metrics_server, duration)
